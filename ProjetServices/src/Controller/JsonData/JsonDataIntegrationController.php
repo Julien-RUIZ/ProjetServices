ffab3275e3ad228c4501867ecc95600b4d72dfdb
Julien RUIZ <julien.ruiz@hotmail.fr>
2024-08-06 15:54:43 +0200

    LastMaj integration data

--- ProjetServices/src/Controller/JsonData/JsonDataIntegrationController.php
+++ ProjetServices/src/Controller/JsonData/JsonDataIntegrationController.php
@@ -27,31 +27,22 @@ class JsonDataIntegrationController extends AbstractController
             $userid = $this->getUser()->getid();
             $link = $this->getParameter('kernel.project_dir');
             $linkJson = $link.'/data/JsonDataExtract/jsondataextract'.$userid.'.json';
-
             if ($filesystem->exists($linkJson)){
                 $datajson = file_get_contents($linkJson);
                 $infos = json_decode($datajson, true);
                 foreach ($infos as $value ){
-
                     $search = $userAddressRepository->findOneBy(['Address'=>$value['userAddress']['Address'], 'postalCode'=>$value['userAddress']['postalCode'], 'number'=>$value['userAddress']['number']]);
-
                     $OneServiceJson = json_encode($value);
                     $deserializeJson = $serializer->deserialize($OneServiceJson, 'App\Entity\service', 'json', [
                         AbstractNormalizer::OBJECT_TO_POPULATE => new UserAddress(),
                     ]);
-                    if(empty($search)){
-                        $deserializeJson->getUserAddress()->setUser($this->getUser());
-                        $entityManager->persist($deserializeJson);
-                    }
                     if(!empty($search)){
                         $deserializeJson->setUserAddress($search);
-                        $deserializeJson->getUserAddress()->setUser($this->getUser());
-                        $entityManager->persist($deserializeJson);
                     }
+                    $deserializeJson->getUserAddress()->setUser($this->getUser());
+                    $entityManager->persist($deserializeJson);
                     $entityManager->flush();
                 }
-
-
                 $this->addFlash('success', 'L\'intégration de vos données a été réalisée avec succès.' );
             }else{
                 $this->addFlash('success', 'Vous ne possédez pas de document pour l\'intégration de données.');
